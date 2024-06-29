ab1927c6797d1411642ecefa14909bd8aa84e951
Teunos2000 <teun.hilbers2000@gmail.com>
2024-06-29 21:21:24 +0200

    Fonts added, global colors added, navbar steps

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ViewEncapsulation } from '@angular/core';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  encapsulation: ViewEncapsulation.None
 })
+
 export class AppComponent {
   title = 'frontend';
 }
